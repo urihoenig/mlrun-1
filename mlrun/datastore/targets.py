d90f52dee10299a9923a5b77c0426d3cf5b16865
Katya Katsenelenbogen <katyak@iguazio.com>
2021-06-14 12:57:44 +0300

    [Feature store] ParquetTarget should be in a single file when partitioned is false (#1009)

--- mlrun/datastore/targets.py
+++ mlrun/datastore/targets.py
@@ -457,7 +457,7 @@ class ParquetTarget(BaseStoreTarget):
         attributes: typing.Dict[str, str] = None,
         after_step=None,
         columns=None,
-        partitioned: bool = False,
+        partitioned: bool = None,
         key_bucketing_number: typing.Optional[int] = None,
         partition_cols: typing.Optional[typing.List[str]] = None,
         time_partitioning_granularity: typing.Optional[str] = None,
@@ -470,6 +470,19 @@ class ParquetTarget(BaseStoreTarget):
                 PendingDeprecationWarning,
             )
             after_step = after_step or after_state
+
+        if partitioned is None:
+            if all(
+                value is None
+                for value in [
+                    key_bucketing_number,
+                    partition_cols,
+                    time_partitioning_granularity,
+                ]
+            ):
+                partitioned = False
+            else:
+                partitioned = True
 
         super().__init__(
             name,
@@ -492,19 +505,7 @@ class ParquetTarget(BaseStoreTarget):
                 f"not {time_partitioning_granularity}."
             )
 
-        self.suffix = (
-            ".parquet"
-            if not partitioned
-            and all(
-                value is None
-                for value in [
-                    key_bucketing_number,
-                    partition_cols,
-                    time_partitioning_granularity,
-                ]
-            )
-            else ""
-        )
+        self.suffix = ".parquet" if not partitioned else ""
 
     _legal_time_units = ["year", "month", "day", "hour", "minute", "second"]
 
@@ -553,6 +554,13 @@ class ParquetTarget(BaseStoreTarget):
                 partition_cols.append(f"${time_unit}")
                 if time_unit == time_partitioning_granularity:
                     break
+
+        if (
+            not self.partitioned
+            and not self._target_path.endswith(".parquet")
+            and not self._target_path.endswith(".pq")
+        ):
+            partition_cols = []
 
         graph.add_step(
             name=self.name or "ParquetTarget",
